b6dd1a102db41af2f43a80e01f13c6bc3924256f
Irawan Kilmer <irawankillmer@gmail.com>
2025-03-16 00:48:22 +0700

    Update responsive

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,6 +3,7 @@
 <!--begin::Head-->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CP2B 4 | Login</title>
     <!--begin::Fonts-->
     <link
